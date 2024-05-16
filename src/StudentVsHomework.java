6c697ccc13c21f0942ab6b41b910a8a4b987a196
Eric Huang <150194591+EricHuang2FG@users.noreply.github.com>
2024-05-16 14:18:51 +0000

    Added Jframe

--- src/StudentVsHomework.java
+++ src/StudentVsHomework.java
@@ -1,8 +1,33 @@
-public class StudentVsHomework {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class StudentVsHomework extends JPanel {
+
+    public static final int SCREEN_WIDTH = 1020;
+    public static final int SCREEN_HEIGHT = 640;
+
+    public StudentVsHomework() {
+        
+    }
+    
     public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("More test");
+        JFrame frame = new JFrame("Student vs. Homework");
+        StudentVsHomework window = new StudentVsHomework();
+        frame.add(window);
+        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true) {
+            Thread.sleep(10)
+        }
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     }
     
 }
