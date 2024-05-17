15e5f552878f53202d27a93be132a6013e654d8c
Eric Huang <150194591+EricHuang2FG@users.noreply.github.com>
2024-05-17 13:53:19 +0000

    Map is drawn on the screen

---
+++ src/Map.java
@@ -0,0 +1,25 @@
+import java.awt.image.*;
+import java.awt.*;
+import java.imageio.*;
+import java.io.*;
+
+public class Map {
+
+    private BufferedImage map = null;
+    private int mapWidth = StudentVsHomework.getScreenWidth();
+    private int mapHeight = StudentVsHomework.getScreenHeight();
+    
+    public Map() {
+        try {
+            this.map = ImageIO.read(new File("res\\map.png"));
+            // this.map = ImageIO.read(new File("..\res\map.png"));
+        } catch (IOException e) {
+            System.out.println("Error loading image: \n" + e);
+        }
+    }
+
+    public void paint(Graphics2D g2d) {
+        g2d.drawImage(this.map, 0, 0, mapWidth, mapHeight, null);
+    }
+    
+}
--- src/StudentVsHomework.java
+++ src/StudentVsHomework.java
@@ -4,11 +4,21 @@ import java.awt.event.*;
 
 public class StudentVsHomework extends JPanel {
 
-    public static final int SCREEN_WIDTH = 1020;
-    public static final int SCREEN_HEIGHT = 640;
+    private static final int SCREEN_WIDTH = 1020;
+    private static final int SCREEN_HEIGHT = 640;
+    private Map map = new Map();
+    private String gameState = "game screen"; // Temporary
 
     public StudentVsHomework() {
         
+    }
+
+    public static int getScreenWidth() {
+        return SCREEN_WIDTH;
+    }
+
+    public static int getScreenHeight() {
+        return SCREEN_HEIGHT;
     }
     
     public static void main(String[] args) throws InterruptedException {
@@ -20,6 +30,15 @@ public class StudentVsHomework extends JPanel {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         while (true) {
+            if (gameState.equals("start screen")) {
+                
+            }
+            if (gameState.equals("game screen")) {
+                
+            }
+            if (gameState.equals("level transition screen")) {
+                
+            }
             Thread.sleep(10);
         }
     }
@@ -28,6 +47,15 @@ public class StudentVsHomework extends JPanel {
         super.paint(g);
         Graphics g2d = (Graphics2D) g;
         // g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if (gameState.equals("start screen")) {
+            
+        }
+        if (gameState.equals("game screen")) {
+            map.paint(g2d);
+        }
+        if (gameState.equals("level transition screen")) {
+
+        }
     }
     
 }
