fd0c689e9d4d9b7fcddb762fa25570c36016dfb1
Eric Huang <150194591+EricHuang2FG@users.noreply.github.com>
2024-05-17 14:20:18 +0000

    Added polygon to Grid class

--- src/Grid.java
+++ src/Grid.java
@@ -6,7 +6,10 @@ public class Grid {
     private int x, y;
     private boolean isOccupied = false;
     private final Color LIGHT_GRAY = new Color(94, 94, 94);
-    private final Color DARK_GRAY = new Color(60, 60, 60);
+    private final Color DARK_GRAY = new Color(55, 55, 55);
+    private int nPoints = 5;
+    private int[] xCoords = new int[nPoints], yCoords = new int[nPoints];
+    private Polygon grid;
     private Color colour;
 
     public Grid(int x, int y, boolean isLightGray) {
@@ -17,6 +20,26 @@ public class Grid {
         } else {
             this.colour = DARK_GRAY;
         }
+        xCoords[0] = this.x;
+        xCoords[1] = this.x + WIDTH;
+        xCoords[2] = this.x + WIDTH;
+        xCoords[3] = this.x;
+        xCoords[4] = this.x;
+        yCoords[0] = this.y;
+        yCoords[1] = this.y;
+        yCoords[2] = this.y + HEIGHT;
+        yCoords[3] = this.y + HEIGHT;
+        yCoords[4] = this.y;
+        grid = new Polygon(xCoords, yCoords, nPoints);
+    }
+
+    public void paint(Graphics2D g2d) {
+        g2d.setColor(this.colour);
+        g2d.fillPolygon(this.grid);
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.BLACK);
+        g2d.drawPolygon(this.grid);
+        g2d.setStroke(new BasicStroke(1));
     }
     
 }
