4d8f5bf4efd09db0d46d56bafa7a06ee60e4aaa2
Eric Huang <150194591+EricHuang2FG@users.noreply.github.com>
2024-05-21 15:14:22 +0000

    Changed map height once again

--- src/Map.java
+++ src/Map.java
@@ -18,7 +18,7 @@ public class Map {
             System.out.println("Error loading image: \n" + e);
         }
         int x = 100;
-        int y = 110;
+        int y = 100;
         for (int i = 0; i < grids.length; i++) {
             for (int j = 0; j < grids[i].length; j++) {
                 if (i % 2 == 0) {
