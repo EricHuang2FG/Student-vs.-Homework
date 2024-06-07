78fd3a645287391a181d626021a7fff40ce756f5
Eric Huang <150194591+EricHuang2FG@users.noreply.github.com>
2024-06-07 15:56:29 +0000

    New animations

--- src/Eraser.java
+++ src/Eraser.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,6 +9,7 @@ public class Eraser extends Tower {
     private static int lastPlaced = 10000000;
     private String imagePath = "res\\towers\\eraser.png";
     private String type = "eraser";
+    private BufferedImage image3 = null;
     private static String cost = "50";
     private static int spawnCoolDown = 15;
 
@@ -18,8 +20,10 @@ public class Eraser extends Tower {
         super.scaledHeight = 100;
         super.type = "eraser";
         String imagePath = "res\\towers\\" + super.type + ".png";
+        String imagePath2 = "res\\towers\\" + "eraser_verydamaged" + ".png";
         try {
             super.image = ImageIO.read(new File(imagePath));
+            image3 = ImageIO.read(new File(imagePath2));
         } catch (IOException e) {
             System.out.println("Error loading image: \n" + e);
         }
@@ -38,6 +42,12 @@ public class Eraser extends Tower {
     }
 
     public void paint(Graphics2D g2d) {
-        g2d.drawImage(this.image, x, y, this.scaledWidth, this.scaledHeight, null);
+        if (this.hitPoints <= 500){
+            g2d.drawImage(this.image3, x, y, this.scaledWidth, this.scaledHeight, null);
+        }
+        else{
+            g2d.drawImage(this.image, x, y, this.scaledWidth, this.scaledHeight, null);
+        }
+
     }
 }
